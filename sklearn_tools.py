9ab3d61581fce7d91e051cb66d0dbefde3592544
ramymohamed-sudo <63137152+ramymohamed-sudo@users.noreply.github.com>
2021-03-08 04:16:37 +0000

    Update sklearn_tools.py

--- sklearn_tools.py
+++ sklearn_tools.py
@@ -1,4 +1,5 @@
 
+
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
@@ -16,6 +17,24 @@ tweet = kgp.get_basic_features(tweeter)
 
 
 """ Classification with Support Vector Machine and TFIDF"""
+# Example of TfidfVectorizer
+tfidf = TfidfVectorizer(
+  norm=l1 #(default =l2)
+  ngram_range=(1,2)		#(default=1,1)
+  analyzer='word'		# tokenization done word by word (‘char’)
+  max_features = 5000, #(limit the dictionary to 5000)
+  stop_words=stopwords.words('english'),
+  max_df=0.5,
+  min_df=0.1,
+  lowercase=True
+)
+feature_matrix = tfidf.fit_transform(body_all_articles)
+feature_matrix.toarray()
+tfidf.get_feature_names() 
+tfidf.labels_
+tfidf.vocabulary_
+fmArray = feature_matrix.toarray() 
+
 text = tweeter['text']
 y = tweeter['target']
 tfidf = TfidfVectorizer()
@@ -30,7 +49,59 @@ print('Classification Report')
 print(classification_report(y_test,y_pred))
 
 
-""" Classification with Support Vector Machine and Word2vec"""
+""" Classification with Support Vector Machine and (Word2vec)"""
+import spacy 
+nlp = spacy.load('en_core_web_lg')        # nlp = spacy.load('en',disable=['parser', 'tagger','ner'])
+tweet['vec'] = tweet['text'].apply(lambda x: nlp('x').vector)
+X = tweet['vec'].to_numpy()
+X = X.reshape(-1,1)
+X = np.concatenate(np.concatenate(X,axis=0),axis=0).reshape(-1,300)           # X.shape = (7613,300)
+# accuract is lower than TFIDF as Spacy is trained for Standard English not Twitter data
+# 
+
+
+
+
+
+
+
+""" Pipeline """
+pass
+>> from sklearn.pipeline import Pipeline
+>> from sklearn.model_selection import GridSearchCV
+>> pipe = Pipeline([(“classifier”, RandomForestClassifier())])
+>> search_space = 			# list of dictionaries 
+
+>> clf = clf = GridSearchCV(pipe,search_space,cv=5,njobs=-1)
+>> clf.fit(X_train,y_train)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+text_clf = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LinearSVC()),])
+
+
+
+
+For the pipeline, i.e., TFIDF and Classifier 
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import LinearSVC
+text_clf = Pipeline([('tfidf', TfidfVectorizer()), ('clf', LinearSVC()),])
+
+# Feed the training data through the pipeline
+text_clf.fit(X_train, y_train)  
+
+""" K-mean Clustering """
+num = 5
+kmeans = KMeans(n_clusters = num, init = 'k-means++', max_iter = 500, n_init = 1)
+kmeans.fit(feature_matrix)
+
+
+
+
+""" Other Models """
+pass
+
 
 
 
